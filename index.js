107f1851241c73960bb553e3e6c8380531b4a0fb
savi <isaacchidera256@gmail.com>
2022-03-07 17:32:47 -0700

    Update User and Delete User

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express =require('express')
+const dotenv=require('dotenv')
+// const MongoClient = require('mongodb').MongoClient
+const mongoose=require('mongoose')
+const bodyParser= require('body-parser')
 
-const app = express();
+const app=express()
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/',require('./router/routes'))
 
-const PORT = process.env.PORT ||  4000;
 
-app.listen(PORT, () => console.log('Listening on http://localhost:' + PORT));
+
+dotenv.config({path:'config.env'})
+const DATA=process.env.DATABASE_URL
+const PORT=process.env.PORT||8080
+
+app.get('/',(req,res)=>{
+    res.json('hello client')
+})
+
+mongoose.connect(DATA,{useUnifiedTopology:true,useNewUrlParser:true})
+.then((client)=> {
+console.log('client connected')})
+
+
+.catch((error)=>console.error(error))
+
+app.listen(PORT,()=>{console.log(`port is working properly http://localhost:${PORT}`)}) 
---
+++ model/Usemodel.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+
+const structure=new mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true},
+    gender:{
+        type:String 
+    },
+    age:{
+        type:Number
+    }
+
+})
+const Usemodel= mongoose.model('SAVI',structure)
+
+module.exports=Usemodel 
