8ae06c372c69fd4d3bf306d4964c908592913074
savi256 <isaacchidera256@gmail.com>
2022-05-05 15:38:15 -0700

    CORS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require("./database/connection");
 const mongoose=require('mongoose')
 const cookies=require('cookie-parser')
+const cors = require('cors')
 // const bodyParser= require('body-parser')
 
 const app=express()
@@ -18,6 +19,11 @@ connectDB();
 //because we can't access the body in the postman we use the middleware below
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(
+    cors({
+        origin:"https:hire-purchase-frontend.vercel.app"
+    })
+)
 
 const port = process.env.port ||  4000;
 app.use('/',require('./router/routes'));
